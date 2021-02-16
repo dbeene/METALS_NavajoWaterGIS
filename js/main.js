66cfaa240acd0639030a549b6d0ee985f8b8debb
Daniel Beene <57543540+dbeene@users.noreply.github.com>
2021-02-16 08:53:54 -0700

    Updated map frame w/Leaflet, added 'main.js'

---
+++ js/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    // Call leaflet map into map frame
+    var map = L.map('map').setView([36.292, -110.090], 8);
+    var HikeBike_HikeBike = L.tileLayer('https://tiles.wmflabs.org/hikebike/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    HikeBike_HikeBike.addTo(map);
+}
